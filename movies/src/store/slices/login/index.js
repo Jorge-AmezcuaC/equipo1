2875c24c2d46401933730ad37cef82e04c8ae2eb
unknown <amezcua.carrillo.jorge.alejandro@gmail.com>
2023-04-30 23:11:47 -0600

    Agregando pantallas para el storage de peliculas y pantallas de login

---
+++ movies/src/store/slices/login/index.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: {},
+  reducers: {},
+});
+
+export default loginSlice.reducer;
